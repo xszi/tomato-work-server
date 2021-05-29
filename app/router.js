2c38808bafbfa475813f3f8726d6d22762486531
xszi <jiuhuaw@126.com>
2021-05-29 17:06:29 +0800

    will change to express

--- app/router.js
+++ app/router.js
@@ -1,8 +1,13 @@
+'use strict'
+
 module.exports = app => {
     const {
         router,
         controller
     } = app;
     router.get('/', controller.home.index);
-    router.get('/news', controller.news.list);
+    // router.get('/news', controller.news.list);
+
+    require('./router/user')(app)
+
 };
