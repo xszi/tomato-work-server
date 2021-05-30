7a024e5e31cb8afdffbd75655c960ba163588f95
xszi <jiuhuaw@126.com>
2021-05-30 16:58:59 +0800

    verify code

--- README.md
+++ README.md
@@ -8,3 +8,27 @@
 * express文件结构（mvc）一般按自己思路组织，而egg会建议官方给定的mvc文件结构
 * context上下文 和 回调request，response，next
 * 个人偏向于喜欢express，一方面灵活一些，另一方面，像webpack和vite这些工程化打包工具都是使用express作为服务，所以学习了express之后可以更方的学习webpack和vite的原理
+
+
+## menul
+
+### 启动mysql数据库
+
+~~~
+cd d:\mysql8\bin
+mysql -u root -p
+~~~
+
+### 关于跨域
+
+如果后台设置httpOnly: true，则前端js不能操作cookie，即domain.cookie不能获取到，只能能浏览器自动带上。
+
+cors库设置 credentials: true 解决关于跨域
+
+### XSRF (跨站请求伪造)
+
+调用后台接口请求返回一个token，保存在localstorage（可以设置过期时间）中，再通过请求header设置，把token带到后台进行身份验证。
+比如：
+```
+{ header['x-token']: 'GSKDKLSLSAADSKLSLS' }
+```
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 
 // const ossClient = require('./utils/oss');
@@ -17,9 +18,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(session({
+    secret: 'secret', // 用来对session数据进行加密的字符串.这个属性值为必须指定的属性。
+    resave: false, // 是指每次请求都重新设置session cookie，假设你的cookie是6000毫秒过期，每次请求都会再设置6000毫秒。
+    saveUninitialized: false, // 是指无论有没有session cookie，每次请求都设置个session cookie ，默认给个标示为 connect.sid。
+    cookie: {
+        maxAge: 1000 * 60 * 3, // Set the effective time of the session, in milliseconds
+    },
+}));
+
 app.use('/api', require('./app/router'))
 
 const port = '8080'
-app.listen(port, function() {
-  console.log(`server start at port ${port}`);
+app.listen(port, function () {
+    console.log(`server start at port ${port}`);
 });
